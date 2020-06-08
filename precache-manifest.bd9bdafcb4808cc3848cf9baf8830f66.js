984727348bace23c2ac2f6530f0710f51f826637
Yang Liu <ethdragon@gmail.com>
2020-06-07 20:58:15 -0400

    Updates

---
+++ precache-manifest.bd9bdafcb4808cc3848cf9baf8830f66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "575ec8bcda66bf5c64871271d3e80d23",
+    "url": "/ruiminsun.com/index.html"
+  },
+  {
+    "revision": "5fed2ef53248c53a5616",
+    "url": "/ruiminsun.com/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "68bde40e69ee13709bdb",
+    "url": "/ruiminsun.com/static/css/main.04c26fb2.chunk.css"
+  },
+  {
+    "revision": "5fed2ef53248c53a5616",
+    "url": "/ruiminsun.com/static/js/2.5c7c3e2d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ruiminsun.com/static/js/2.5c7c3e2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68bde40e69ee13709bdb",
+    "url": "/ruiminsun.com/static/js/main.71868d29.chunk.js"
+  },
+  {
+    "revision": "8c7a8745b5420f718d3f",
+    "url": "/ruiminsun.com/static/js/runtime-main.20925915.js"
+  },
+  {
+    "revision": "73e6dd21c570d90492403c691dee2e13",
+    "url": "/ruiminsun.com/static/media/profile_s.73e6dd21.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2a1f234fcbd70b29fff792f67a233ce1.js"
+  "/ruiminsun.com/precache-manifest.bd9bdafcb4808cc3848cf9baf8830f66.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ruiminsun.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
