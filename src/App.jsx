ed65dc82333eea8a605166d1c033a9193ccf63fa
Ruimin Sun <gracesrm2012@gmail.com>
2020-06-11 23:12:26 -0400

    domain name

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Router basename="ruiminsun.com">
+      <Router>
       <NavBar/>
         <Body>
           <Switch>
