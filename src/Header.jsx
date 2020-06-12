74a6273514b8f7bb8277364e57a2cd407fbf512e
Ruimin Sun <gracesrm2012@gmail.com>
2020-06-12 18:48:11 -0400

    meta changes

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import './App.css';
 import { NavLink } from 'react-router-dom';
-import cv from './Assets/ruimin-neu-cv.pdf'
+import cv from './Downloads/CV/ruimin-neu-cv.pdf'
 
 
 const Navigation = () => {
