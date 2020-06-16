3395a8e4a59c567a32a1ea41270a1006e124b62c
Ruimin Sun <gracesrm2012@gmail.com>
2020-06-16 11:34:41 -0400

    new master with build files, source files will be in *new* branch

---
+++ precache-manifest.ee690362bc291c1484209323f633a2a2.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd7cdaff66da578cad9b1f8e8598e6fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddfc0f18b88a51503b7d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "40c2e27c0068a5b65562",
+    "url": "/static/css/main.d4a7e1e8.chunk.css"
+  },
+  {
+    "revision": "ddfc0f18b88a51503b7d",
+    "url": "/static/js/2.0f75e179.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0f75e179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40c2e27c0068a5b65562",
+    "url": "/static/js/main.67e45389.chunk.js"
+  },
+  {
+    "revision": "e731ebcf83609e71b2bd",
+    "url": "/static/js/runtime-main.9aeaae99.js"
+  },
+  {
+    "revision": "1f482dd2bdeede3a5bb72a85a52f5fab",
+    "url": "/static/media/ddift_2019.1f482dd2.pdf"
+  },
+  {
+    "revision": "62f9b2b0c65b123712209bee4d53a01d",
+    "url": "/static/media/dsc_2017.62f9b2b0.pdf"
+  },
+  {
+    "revision": "6ef7317ac9a3ff2a186244bfaf5f08ab",
+    "url": "/static/media/hotos_2015.6ef7317a.pdf"
+  },
+  {
+    "revision": "c7c99cac8c93b7cccfabb35ba9e8d3d3",
+    "url": "/static/media/icdcs_2020.c7c99cac.pdf"
+  },
+  {
+    "revision": "915c45a6ab5a268060e75df939ce532e",
+    "url": "/static/media/issre_2016.915c45a6.pdf"
+  },
+  {
+    "revision": "6898fa6904c7bc72df6b7068b313ff8d",
+    "url": "/static/media/login_2015.6898fa69.pdf"
+  },
+  {
+    "revision": "72c763e3fb18801c55d4124ca2cbe9e7",
+    "url": "/static/media/modelxray_preprint.72c763e3.pdf"
+  },
+  {
+    "revision": "73e6dd21c570d90492403c691dee2e13",
+    "url": "/static/media/profile_s.73e6dd21.jpg"
+  },
+  {
+    "revision": "b1e8f52eabb976f2fd2d0d715d97f1f7",
+    "url": "/static/media/propedeutica_2018.b1e8f52e.pdf"
+  },
+  {
+    "revision": "016897ea87676f576cb2d52939214593",
+    "url": "/static/media/ruimin-neu-cv.016897ea.pdf"
+  },
+  {
+    "revision": "41eb00631da02e9f0e7641974b9b2149",
+    "url": "/static/media/sok_preprint.41eb0063.pdf"
+  },
+  {
+    "revision": "10dc1f0e9b2cfa4d3f2cfd818a067620",
+    "url": "/static/media/tdsc_2020.10dc1f0e.pdf"
+  }
+]);
