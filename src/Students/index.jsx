056c924c0198be8d61c9e7d4504953fa34210332
gracesrm <gracesrm2012@gmail.com>
2022-10-05 14:34:26 -0400

    update with students

--- src/Students/index.jsx
+++ src/Students/index.jsx
@@ -1,24 +1,47 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
+import Paragraph from '../ParagraphContainer';
 import ParagraphContainer from '../ParagraphContainer';
 import '../AppVanillaHtml.css';
 
 
 const Students = () => (
     <Container>
-        <Row
-            className="justify-content-center"
-            style={{paddingTop: "1.5em"}}>
-                {/* <h3>Guest Lecturer</h3> */}
+        <Row className="justify-content-center" style={{paddingTop: "1.5em"}}>
+                <h3>PhD Students</h3>
         </Row>
-        <ParagraphContainer>
+        <Paragraph>
+            <ol >
+                <li className="text-left li_padding_bottom"> Mohammed Al Duniawi </li>     
+                <li className="text-left li_padding_bottom"> Qiming Guo </li>       
+            </ol>
+        </Paragraph>
+        <Row className="justify-content-center" style={{paddingTop: "1.5em"}}>
+                <h3>Master Students</h3>
+        </Row>
+        <Paragraph>
+            <ol >
+                {/* <li className="text-left li_padding_bottom"> Mohammed Al Duniawi </li>     
+                <li className="text-left li_padding_bottom"> Qiming Guo </li>        */}
+            </ol>
+        </Paragraph>
+        <Row className="justify-content-center" style={{paddingTop: "1.5em"}}>
+                <h3>Undergraduate Students</h3>
+        </Row>
+        <Paragraph>
+            <ol >
+                {/* <li className="text-left li_padding_bottom"> Mohammed Al Duniawi </li>     
+                <li className="text-left li_padding_bottom"> Qiming Guo </li>        */}
+            </ol>
+        </Paragraph>
+        {/* <ParagraphContainer>
             <ol>
                 
                 
                    
             </ol>
-        </ParagraphContainer>
+        </ParagraphContainer> */}
     </Container>
 );
 
