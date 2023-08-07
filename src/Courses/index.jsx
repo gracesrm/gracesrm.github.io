8e879535063792c4d38c885997afd23977166925
gracesrm <gracesrm2012@gmail.com>
2023-08-07 00:39:06 -0400

    change course url

--- src/Courses/index.jsx
+++ src/Courses/index.jsx
@@ -5,21 +5,10 @@ import '../AppVanillaHtml.css';
 const Courses = () => (
 
         <div>
-            <h3><a href="http://ruiminsun-courses.s3-website-us-east-1.amazonaws.com/" target="_blank">Security for ML</a></h3>
+            <h3><a href="https://sun-ris3.github.io/" target="_blank">Security for ML</a></h3>
         </div>
       
 );
 
-// function myredirect() {
-//     return (
-//         <div>
-//           <p>
-//             <a href="http://ruiminsun-courses.s3-website-us-east-1.amazonaws.com/" target="_blank">
-//               Click to open HereWeCode (new tab)
-//             </a>
-//           </p>
-//         </div>
-//       )
-//   }
 
 export default Courses;
