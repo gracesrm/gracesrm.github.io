26fd78a0855ab290d745fdff8224cc0d42f9a877
gracesrm <gracesrm2012@gmail.com>
2023-09-16 21:58:45 -0400

    add students

--- src/Students/index.jsx
+++ src/Students/index.jsx
@@ -12,27 +12,36 @@ const Students = () => (
         </Row>
         <Paragraph>
             <ol >
-                <li className='text-left li_padding_bottom'> Samin Yasar Chowdhury </li>
-                <li className="text-left li_padding_bottom"> Mohammed Al Duniawi </li>     
-                <li className="text-left li_padding_bottom"> Qiming Guo </li>       
+                <li className='text-left li_padding_bottom'> Samin Yasar Chowdhury (Spring'23 - present)</li>
+                <li className="text-left li_padding_bottom"> Mohammed Al Duniawi (Fall'23 - present)</li>     
+                <li className="text-left li_padding_bottom"> Qiming Guo (Fall'23 - present) </li>       
             </ol>
         </Paragraph>
         <Row className="justify-content-center" style={{paddingTop: "1.5em"}}>
-                <h3>Master Students</h3>
         </Row>
         <Paragraph>
             <ol >
-                {/* <li className="text-left li_padding_bottom"> Mohammed Al Duniawi </li>     
-                <li className="text-left li_padding_bottom"> Qiming Guo </li>        */}
+            <h3>Master Students</h3>
+                <li className='text-left li_padding_bottom'> Tushar Nayan (Summer'23 - present) </li>
+                <li className='text-left li_padding_bottom'> Sai Siva Prabhu Teja Kondapalli (Fall'23)</li>
+                <li className="text-left li_padding_bottom"> Srimant Kumar Mohanty (Summer'23) </li>    
             </ol>
         </Paragraph>
         <Row className="justify-content-center" style={{paddingTop: "1.5em"}}>
-                <h3>Undergraduate Students</h3>
         </Row>
         <Paragraph>
             <ol >
-                {/* <li className="text-left li_padding_bottom"> Mohammed Al Duniawi </li>     
-                <li className="text-left li_padding_bottom"> Qiming Guo </li>        */}
+            <h3>Undergraduate Students</h3>
+                <li className="text-left li_padding_bottom"> Adonis Paulino (Summer'23) </li> 
+                <li className="text-left li_padding_bottom"> Danielle Leon (Fall'23) </li> 
+            </ol>
+        </Paragraph>
+        <Row className="justify-content-center" style={{paddingTop: "1.5em"}}>
+        </Row>
+        <Paragraph>
+            <ol >
+            <h3>K-12 Students</h3>
+                <li className="text-left li_padding_bottom"> Eric Xu (Summer'23) </li> 
             </ol>
         </Paragraph>
         {/* <ParagraphContainer>
