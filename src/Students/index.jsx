5522292a67d13e759ad3cd5f7fa8278d7583ee50
gracesrm <gracesrm2012@gmail.com>
2024-01-05 17:14:38 -0500

    update student info

--- src/Students/index.jsx
+++ src/Students/index.jsx
@@ -12,9 +12,10 @@ const Students = () => (
         </Row>
         <Paragraph>
             <ol >
+                <li className="text-left li_padding_bottom"> Sohely Jahan (Spring'24 - present)</li>   
                 <li className='text-left li_padding_bottom'> Samin Yasar Chowdhury (Spring'23 - present)</li>
-                <li className="text-left li_padding_bottom"> Mohammed Al Duniawi (Fall'23 - present)</li>     
-                <li className="text-left li_padding_bottom"> Qiming Guo (Fall'23 - present) </li>       
+                <li className="text-left li_padding_bottom"> Mohammed Al Duniawi (Fall'22 - present)</li>     
+                {/* <li className="text-left li_padding_bottom"> Qiming Guo (Fall'22 - Fall'23) </li>        */}
             </ol>
         </Paragraph>
         <Row className="justify-content-center" style={{paddingTop: "1.5em"}}>
@@ -22,8 +23,8 @@ const Students = () => (
         <Paragraph>
             <ol >
             <h3>Master Students</h3>
-                <li className='text-left li_padding_bottom'> Tushar Nayan (Summer'23 - present) </li>
                 <li className='text-left li_padding_bottom'> Sai Siva Prabhu Teja Kondapalli (Fall'23)</li>
+                <li className='text-left li_padding_bottom'> Tushar Nayan (Summer'23 - present) </li>
                 <li className="text-left li_padding_bottom"> Srimant Kumar Mohanty (Summer'23) </li>    
             </ol>
         </Paragraph>
@@ -32,8 +33,9 @@ const Students = () => (
         <Paragraph>
             <ol >
             <h3>Undergraduate Students</h3>
-                <li className="text-left li_padding_bottom"> Adonis Paulino (Summer'23) </li> 
+                <li className="text-left li_padding_bottom"> Emily Lofaro (Fall'23, Co-advised with Dr. Hadi Amini) </li> 
                 <li className="text-left li_padding_bottom"> Danielle Leon (Fall'23) </li> 
+                <li className="text-left li_padding_bottom"> Adonis Paulino (Summer'23) </li> 
             </ol>
         </Paragraph>
         <Row className="justify-content-center" style={{paddingTop: "1.5em"}}>
